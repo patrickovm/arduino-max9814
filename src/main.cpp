a46a9dbcfc3d1d28274ccdad5fed9621169e1776
Patrick Monteiro <patrickom21@gmail.com>
2024-07-14 20:51:59 -0300

    feat: read sensor values
    change baud rate to 115200

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* 1s fo each measurement */
+const int sample_interval = 1000;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+unsigned int sample;
+
+void setup() { Serial.begin(115200); }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned long sample_interval_start = millis();
+  unsigned int peak_to_peak = 0;
+  unsigned int max_signal = 0;
+  unsigned int min_signal = 1024;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (millis() - sample_interval_start < sample_interval) {
+    /* sample analog value read on A0 */
+    sample = analogRead(A0);
+
+    if (sample < min_signal) {
+      if (sample > max_signal) {
+        /* max_signal goes from 0 -> sample */
+        max_signal = sample;
+      } else if (sample < min_signal) {
+        min_signal = sample;
+      }
+    }
+  }
+
+  /* peak to peak amplitude */
+  peak_to_peak = max_signal - min_signal;
+
+  double volts = (peak_to_peak * 5.0) / 1024.0;
+
+  Serial.print("Max signal: ");
+  Serial.println(max_signal);
+
+  Serial.print("Min signal: ");
+  Serial.println(min_signal);
+
+  Serial.print("Volts: ");
+  Serial.println(volts);
+  Serial.println();
+
+  delay(1000);
 }
